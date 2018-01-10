8471a73bab463e05d085d4caa342aa2619f50756
Remy Ponso <remy@central>
2018-01-10 21:22:07 -0200

    production version, with dev dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) =>{
  return {
   entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public/dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -43,6 +43,7 @@ module.exports = (env) =>{
     devtool: isProd ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
+      publicPath: '/dist/',
       historyApiFallback: true
     }
   };
