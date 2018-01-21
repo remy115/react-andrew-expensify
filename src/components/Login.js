a23b40c451369554892cbd9a7dc10575d144b933
Remy Ponso <remy@central>
2018-01-21 18:08:08 -0200

    lkjklj

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,12 +26,12 @@ export class Login extends React.Component {
 
         this.setState(()=>({
             [name]:value
-        }))))));
+        }));
     }
 
     render() {
         return (
-            <div className="box-layout" >
+            <div className="box-layout">
                 <div className="box-layout__box">
                     <h2 className="box-layout__title">Expensify</h2>
                     <p>It's time to get your expenses under control</p>
